96908e84ae21f60e97705a9606c30f082b18baf0
cristhiansantosnazar <csantosn@unemi.edu.ec>
2025-07-21 17:34:59 -0500

    Header con imagen y grip

--- src/pagina/header.jsx
+++ src/pagina/header.jsx
@@ -1,11 +1,73 @@
 import { Link } from 'react-router-dom';
+import imgFondo from '../assets/image-web-3-desktop.jpg';
 export default function Menu() {
   return <div>
-    <Link className='font-bold text-center text-3xl text-red bg-emerald-300' to="/menu">Menu</Link>
-    <Link className='font-bold text-center text-3xl text-red bg-green-800' to="/producto">Producto</Link>
     
-    <h1>Pagina 1</h1>
-    <h1>Pagina 2</h1>
-    <h1>Pagina 3</h1>
+    
+    <header className='text-gray-500 body-font'>
+        <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+            <svg className='h-10 w-10' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512 ">
+            <path d="M20.8 34c16.5-6.2 35 2.2 41.2 18.7l110.2 294L257.3 55c4-13.7 16.5-23 30.7-23s26.7 9.4 30.7 23l85.1 291.7L514 52.8c6.2-16.5 24.6-24.9 41.2-18.7s24.9 24.7 18.7 41.2l-144 384c-4.8 12.9-17.4 21.3-31.2 20.7s-25.7-9.8-29.5-23L288 178.3 206.7 457c-3.9 13.2-15.8 22.5-29.5 23s-26.3-7.8-31.2-20.7L2 75.2C-4.2 58.7 4.2 40.2 20.8 34z"/></svg>
+            
+            <nav className="md:ml-auto flex flex-wrap items-center  justify-center space-x-6 text-3xl">
+                <Link to="/menu" className="text-gray-500 hover:text-cyan-400 transition-colors duration-300 cursor-pointer">
+                    Inicio
+                </Link>
+                <Link to="/producto" className="text-gray-500 hover:text-cyan-400 transition-colors duration-300 cursor-pointer">
+                    Producto
+                </Link>
+                <Link to="/producto" className="text-gray-500 hover:text-cyan-400 transition-colors duration-300 cursor-pointer">
+                    Contacto
+                </Link>
+
+            
+            </nav>
+        </div>
+        
+    </header>
+
+    <div className="grid grid-cols-7 grid-rows-5 gap-4 m-2 ">
+        <div className="col-span-4 row-span-3 bg-amber-900 overflow-hidden rounded-lg ">
+            <img src={imgFondo} alt="Fondo" className="w-full h-full object-cover " />
+        </div>
+        <div className="col-span-3 row-span-5 col-start-5 bg-blue-950 overflow-hidden rounded-lg ">
+            <div className='m-8'>
+                <h1 className='text-6xl font-bold text-amber-400'>NEW</h1>
+            </div>
+            <div className='m-8'>
+                <h2 className='text-white font-bold text-2xl'>Hydrogen VS Electric Cars</h2>
+                <div className="flex-grow flex items-center justify-center">
+                    <p className=" text-black text-2xl ">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A ab, sed dolor eligendi reiciendis, pariatur maxime dolore tempore, molestiae necessitatibus repudiandae aliquid possimus? Error, eius. Fugiat aut sunt aliquid labore?
+                    </p>
+                </div>
+                <hr className='border-2 border-white w-full mt-6' />
+            </div>
+            
+            <div className='m-8'>
+                <h2 className='text-white font-bold text-2xl'>Hydrogen VS Electric Cars</h2>
+                <div className="flex-grow flex items-center justify-center">
+                    <p className=" text-black text-2xl ">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A ab, sed dolor eligendi reiciendis, pariatur maxime dolore tempore, molestiae necessitatibus repudiandae aliquid possimus? Error, eius. Fugiat aut sunt aliquid labore?
+                    </p>
+                </div>
+                <hr className='border-2 border-white w-full mt-6' />
+            </div>
+        </div>
+        <div className="col-span-2 row-span-2 row-start-4  overflow-hidden rounded-lg ">
+            <h1 className='text-6xl font-bold text-black'>The bright</h1>
+            <br />
+            <h1 className='text-6xl font-bold text-black'>Future of </h1>
+            <br />
+            <h1 className='text-6xl font-bold text-black'>Web 3.0? </h1>
+        </div>
+        <div className="col-span-2 row-span-2 col-start-3 row-start-4 flex flex-col justify-end">
+            <p className='text-gray-500 text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, earum, culpa placeat corrupti quasi accusamus, dolor velit sint consequatur fugiat commodi sit obcaecati officiis adipisci animi reprehenderit libero saepe inventore!</p>
+            <button className='bg-amber-600 mt-4 text-[20px] text-black font-bold p-2 rounded-lg w-sm cursor-pointer'>
+                Read More
+            </button>
+        </div>
+    </div>
+    
   </div>
 }
--- src/pagina/producto.jsx
+++ src/pagina/producto.jsx
@@ -1,10 +1,83 @@
 import { Link } from 'react-router-dom';
+
 export default function Producto() {
-  return <div>
-    <Link className='font-bold text-center text-3xl text-red bg-emerald-300' to="/menu">Menu</Link>
-    <Link className='font-bold text-center text-3xl text-red bg-green-800' to="/producto">Producto</Link>
-    
-    <h1>Cebolla</h1>
-    <h1>Pagina 2</h1>
-  </div>
+  return (
+    <div>
+      <header className="bg-black bg-opacity-80 backdrop-blur-md shadow-lg sticky top-0 z-50 shadow-cyan-400/50">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <nav className="md:ml-auto flex flex-wrap items-center justify-center space-x-6 text-3xl">
+            <Link
+              to="/menu"
+              className="text-gray-300 hover:text-cyan-400 transition-colors duration-300 cursor-pointer"
+            >
+              Inicio
+            </Link>
+            <Link
+              to="/producto"
+              className="text-gray-300 hover:text-cyan-400 transition-colors duration-300 cursor-pointer"
+            >
+              Producto
+            </Link>
+          </nav>
+        </div>
+      </header>
+
+    <div className="p-10 text-center flex flex-row justify-around items-center">
+        <span className="inline-block w-6 h-6 text-red-500">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-full h-full fill-current">
+                <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/>
+            </svg>
+        </span>
+
+        <h1 className="text-4xl font-bold text-cyan-400 mb-6">Youtuve</h1>
+
+        <h1 className="text-4xl font-bold text-cyan-400 mb-6">Página 2</h1>
+
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            className="w-16 h-16 text-cyan-400 fill-current"
+        >
+            <path d="M0 64C0 28.7 28.7 0 64 0L352 0c35.3 0 64 28.7 64 64l0 64c0 35.3-28.7 64-64 64L64 192c-35.3 0-64-28.7-64-64L0 64zM160 352c0-17.7 14.3-32 32-32l0-16c0-44.2 35.8-80 80-80l144 0c17.7 0 32-14.3 32-32l0-32 0-90.5c37.3 13.2 64 48.7 64 90.5l0 32c0 53-43 96-96 96l-144 0c-8.8 0-16 7.2-16 16l0 16c17.7 0 32 14.3 32 32l0 128c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32l0-128z"/>
+        </svg>
+
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            className="w-16 h-16 text-cyan-400 fill-current"
+        >
+            <path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
+        </svg>
+    </div>
+    <div className="flex items-center space-x-4 mb-6">
+        <span className="inline-block w-6 h-6 text-red-500">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-full h-full fill-current">
+            <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/>
+            </svg>
+        </span>
+
+        <h1 className="text-4xl font-bold text-cyan-400">YouTube</h1>
+    </div>
+    <div className="flex items-center space-x-4 mb-6">
+        <span className="inline-block w-6 h-6 text-red-500">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-full h-full fill-current">
+            <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/>
+            </svg>
+        </span>
+
+        <h1 className="text-4xl font-bold text-cyan-400">YouTube</h1>
+    </div>
+    <div className="flex items-center space-x-4 mb-6">
+        <span className="inline-block w-6 h-6 text-red-500">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-full h-full fill-current">
+            <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/>
+            </svg>
+        </span>
+
+        <h1 className="text-4xl font-bold text-cyan-400">YouTube</h1>
+    </div>
+
+
+    </div>
+  );
 }
